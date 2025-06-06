871e8e255a04503aecd61785e9b0865ce393a148
msanjai2005 <msanjai1012@gmail.com>
2025-06-06 13:34:27 +0530

    first commit

--- script.js
+++ script.js
@@ -76,27 +76,33 @@ for (i = 0; i<2; i++){
 const projects = [
       {
         title: "Restuarant Web APP",
-        description: "A responsive restaurant website with menu display and table reservation features, built for a smooth user experience."
+        description: "A responsive restaurant website with menu display and table reservation features, built for a smooth user experience.",
+        link: "https://github.com/msanjai2005/Rock_Paper_Scissors"
       },
       {
         title: "Recipe App",
-        description: "A simple and responsive app to browse and view recipes with ingredients and instructions. Built for easy meal planning and inspiration."
+        description: "A simple and responsive app to browse and view recipes with ingredients and instructions. Built for easy meal planning and inspiration.",
+        link:"https://github.com/msanjai2005" 
       },
       {
         title: "Guess The Number Game",
-        description: "An interactive number guessing game with instant feedback. Enhances logical thinking in a fun and engaging way."
+        description: "An interactive number guessing game with instant feedback. Enhances logical thinking in a fun and engaging way.",
+        link:"https://github.com/msanjai2005" 
       },
       {
         title: "Rock-Paper-Scissor Game",
-        description: "A classic rock-paper-scissors game with score tracking. Features clean UI and simple game logic for quick play."
+        description: "A classic rock-paper-scissors game with score tracking. Features clean UI and simple game logic for quick play.",
+        link:"https://github.com/msanjai2005" 
       },
       {
         title: "To-Do-List App",
-        description: "A lightweight app to manage daily tasks with add, delete, and mark-complete features. Designed for productivity and ease of use."
+        description: "A lightweight app to manage daily tasks with add, delete, and mark-complete features. Designed for productivity and ease of use.",
+        link:"https://github.com/msanjai2005" 
       },
       {
         title: "Calculator App",
-        description: "A basic calculator for performing arithmetic operations. Built with a clean interface and real-time input handling."
+        description: "A basic calculator for performing arithmetic operations. Built with a clean interface and real-time input handling.",
+        link:"https://github.com/msanjai2005" 
       }
     ];
 
@@ -106,7 +112,7 @@ const projects = [
       <div class="project">
         <h2>${project.title}</h2>
         <p>${project.description}</p>
-        <button><a href="https://github.com/msanjai2005" target=_blank>Learn more <i class="fa-solid fa-arrow-right"></i></a></button>
+        <button><a href="${project.link}" target=_blank>Learn more <i class="fa-solid fa-arrow-right"></i></a></button>
       </div>
     `).join("");
 
